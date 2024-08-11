183ddd16200114519f8e0fa4107bcfe2f105bf38
MartinZilber <48520155@est.ort.edu.ar>
2024-08-11 14:07:06 -0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TP06_Zilbersztein.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
